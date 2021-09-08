c2aea5bb66278195ff629043781eee880e4a1a5f
Matheus Henrique <matheus.181137@gmail.com>
2021-09-07 22:51:52 -0400

    fix: use uppercase in css to grant accessibility for screen readers

--- src/components/Shop/ShopHeader/index.tsx
+++ src/components/Shop/ShopHeader/index.tsx
@@ -4,7 +4,7 @@ export const ShopHeader = () => {
   return (
     <>
     <section className={style.shopHeader}>
-      <h2 className={style.promotion}>PROMOÇÃO</h2>
+      <h2 className={style.promotion}>promoção</h2>
       <p className={style.product}>9 produtos</p>
     </section>
     <hr className={style.line} />
